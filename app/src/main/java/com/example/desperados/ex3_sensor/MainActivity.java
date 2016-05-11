4e520f6b69deb924614ac66240995d36c7b9a090
dest_0 <dest_0@tut.by>
2016-05-11 12:06:46 +0200

    Output of accelerometer values without a filter

--- app/src/main/java/com/example/desperados/ex3_sensor/MainActivity.java
+++ app/src/main/java/com/example/desperados/ex3_sensor/MainActivity.java
@@ -1,13 +1,79 @@
 package com.example.desperados.ex3_sensor;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private static TextView vt_x, vt_y, vt_z, vt_m;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            // Success! There's a accelerometer.
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        }
+        else {
+            // Failure! No accelerometer.
+        }
+
+        vt_x = (TextView)findViewById(R.id.vt_x);
+        vt_y = (TextView)findViewById(R.id.vt_y);
+        vt_z = (TextView)findViewById(R.id.vt_z);
+        vt_m = (TextView)findViewById(R.id.vt_m);
+
+
     }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        // In this example, alpha is calculated as t / (t + dT),
+        // where t is the low-pass filter's time-constant and
+        // dT is the event delivery rate.
+
+        //final float alpha = 0.8;
+
+        // Isolate the force of gravity with the low-pass filter.
+       // gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        //gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        //gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+        // Remove the gravity contribution with the high-pass filter.
+       // linear_acceleration[0] = event.values[0] - gravity[0];
+        //linear_acceleration[1] = event.values[1] - gravity[1];
+        //linear_acceleration[2] = event.values[2] - gravity[2];
+
+        vt_x.setText(Float.toString(event.values[0]));
+        vt_y.setText(Float.toString(event.values[1]));
+        vt_z.setText(Float.toString(event.values[2]));
+        //Log.i("i am triggered", ""+event.values[0]);
+
+    }
+
+
+
 }
