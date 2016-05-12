0fc4d3c29e790c3f97cb008efb3ac8e6a556f995
dest_0 <dest_0@tut.by>
2016-05-13 00:18:39 +0200

    Seekbar+sampling rate

--- app/src/main/java/com/example/desperados/ex3_sensor/MainActivity.java
+++ app/src/main/java/com/example/desperados/ex3_sensor/MainActivity.java
@@ -1,3 +1,7 @@
+//The following sources were used as a reference:
+//https://developer.android.com/guide/topics/sensors/sensors_motion.html
+//https://examples.javacodegeeks.com/android/core/widget/seekbar/android-seekbar-example/
+
 package com.example.desperados.ex3_sensor;
 
 import android.app.Activity;
@@ -10,12 +14,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private static TextView vt_x, vt_y, vt_z, vt_m;
+    private SeekBar sb_sensor;
+
+
+
+
+
 
 
 
@@ -23,26 +36,67 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeVariables();
 
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
             // Success! There's a accelerometer.
             mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-        }
-        else {
+        } else {
             // Failure! No accelerometer.
         }
 
-        vt_x = (TextView)findViewById(R.id.vt_x);
-        vt_y = (TextView)findViewById(R.id.vt_y);
-        vt_z = (TextView)findViewById(R.id.vt_z);
-        vt_m = (TextView)findViewById(R.id.vt_m);
+        vt_m.setText("Covered: " + sb_sensor.getProgress() + "/" + sb_sensor.getMax());
+
+        sb_sensor.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
 
 
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                vt_m.setText("Covered: " + progress + "/" + seekBar.getMax());
+                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+                mSensorManager.unregisterListener(MainActivity.this);
+                mSensorManager.registerListener(MainActivity.this, mSensor, (100-progress)*2000 );
+                Log.i("Progress on stop", ""+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+                //mSensorManager.unregisterListener(MainActivity.this);
+                //mSensorManager.registerListener(MainActivity.this, mSensor, 200000);
+                Log.i("Progress on start", ""+progress);
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+               // Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+                //Log.i("i am triggered", ""+mSensor.getMinDelay());
+                //mSensorManager.unregisterListener(MainActivity.this);
+                //mSensorManager.registerListener(MainActivity.this, mSensor,progress*200 );
+                Log.i("Progress on change", ""+progress);
+
+            }
+
+
+
+
+
+        });
     }
+
+
+
+
+
+
+
 
     @Override
     public final void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -70,10 +124,34 @@ public class MainActivity extends Activity implements SensorEventListener {
         vt_x.setText(Float.toString(event.values[0]));
         vt_y.setText(Float.toString(event.values[1]));
         vt_z.setText(Float.toString(event.values[2]));
+        //vt_m.setText(Double.toString(Math.sqrt(Math.pow(event.values[0], 2) + Math.pow(event.values[1], 2) + Math.pow(event.values[2], 2))));
+
         //Log.i("i am triggered", ""+event.values[0]);
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+
+
+    private void initializeVariables() {
+        vt_x = (TextView)findViewById(R.id.vt_x);
+        vt_y = (TextView)findViewById(R.id.vt_y);
+        vt_z = (TextView)findViewById(R.id.vt_z);
+        vt_m = (TextView)findViewById(R.id.vt_m);
+        sb_sensor = (SeekBar) findViewById(R.id.sb_sensor);
+
+    }
 
 
 }
