b69d3e3c7a1cdc3219b376a0d40002ce7644d2b3
dest_0 <dest_0@tut.by>
2016-05-15 14:03:54 +0200

    Setting up sensor reading visualizations properly.

--- app/src/main/java/com/example/desperados/ex3_sensor/SensorGraph.java
+++ app/src/main/java/com/example/desperados/ex3_sensor/SensorGraph.java
@@ -16,8 +16,9 @@ public class SensorGraph extends View{
 
     // setup initial color
     private final int paintColor = Color.RED;
+    private final int textColor = Color.WHITE;
     // defines paint and canvas
-    private Paint drawPaint;
+    private Paint drawPaint, textPaint;
     public float x1, x2, x3, x4, xcent, ycent;
 
 
@@ -37,12 +38,16 @@ public class SensorGraph extends View{
 
         canvas.drawARGB(255, 0, 0, 0);
         drawPaint.setColor(Color.RED);
+        canvas.drawText("X:", 10, 100, textPaint);
         canvas.drawLine(xcent,100,xcent+x1*10,100,drawPaint);
         drawPaint.setColor(Color.GREEN);
+        canvas.drawText("Y:", 10, 200, textPaint);
         canvas.drawLine(xcent,200,xcent+x2*10,200,drawPaint);
         drawPaint.setColor(Color.BLUE);
+        canvas.drawText("Z:", 10, 300, textPaint);
         canvas.drawLine(xcent,300,xcent+x3*10,300,drawPaint);
         drawPaint.setColor(Color.WHITE);
+        canvas.drawText("Magnitude:", 10, 400, textPaint);
         canvas.drawLine(xcent,400,xcent+x4*10,400,drawPaint);
 
     }
@@ -53,10 +58,17 @@ public class SensorGraph extends View{
         drawPaint = new Paint();
         drawPaint.setColor(paintColor);
         drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(5);
+        drawPaint.setStrokeWidth(15);
         drawPaint.setStyle(Paint.Style.STROKE);
         drawPaint.setStrokeJoin(Paint.Join.ROUND);
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        textPaint = new Paint();
+        textPaint.setColor(textColor);
+        textPaint.setTextSize(50);
+
+
+
     }
 
 }
